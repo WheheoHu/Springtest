2b4641369d54949faf2e57b99649e103f3fccba0
文昊 胡 <hwh19981124@hotmail.com>
2019-09-26 09:14:05 +0800

    remove beanfactory

--- src/test/client/Client.java
+++ src/test/client/Client.java
@@ -2,15 +2,18 @@ package test.client;
 
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import test.dao.CustomerDao;
 import test.service.CustomerService;
 import test.service.CustomerServiceImpl;
 
 public class Client {
     public static void main(String[] args) {
+        //use beanfactory
+//        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring-config.xml"));
+//        CustomerService customerService_2 = (CustomerServiceImpl) beanFactory.getBean("CustomerService");
+//        customerService_2.saveCustomer();
+
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
         CustomerService customerService = (CustomerServiceImpl) classPathXmlApplicationContext.getBean("CustomerService");
-       // CustomerDao customerDao = (CustomerDao) classPathXmlApplicationContext.getBean("CustomerDao");
         customerService.saveCustomer();
     }
 }
--- src/test/service/CustomerServiceImpl.java
+++ src/test/service/CustomerServiceImpl.java
@@ -6,6 +6,9 @@ import test.dao.CustomerDao;
 import test.dao.CustomerDaoImpl;
 
 public class CustomerServiceImpl implements CustomerService {
+    public CustomerServiceImpl(){
+        System.out.println("bean created!");
+    }
 
     private CustomerDao customerDaoImpl=new CustomerDaoImpl();
     public void saveCustomer() {
