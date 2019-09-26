1369d6d88d9995bfb4bea93114378da1603b4d61
文昊 胡 <hwh19981124@hotmail.com>
2019-09-26 09:23:45 +0800

    remove beanfactory!!

--- src/test/client/Client.java
+++ src/test/client/Client.java
@@ -7,10 +7,7 @@ import test.service.CustomerServiceImpl;
 
 public class Client {
     public static void main(String[] args) {
-        //use beanfactory
-//        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring-config.xml"));
-//        CustomerService customerService_2 = (CustomerServiceImpl) beanFactory.getBean("CustomerService");
-//        customerService_2.saveCustomer();
+
 
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
         CustomerService customerService = (CustomerServiceImpl) classPathXmlApplicationContext.getBean("CustomerService");
