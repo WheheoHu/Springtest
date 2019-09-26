7ba16a816cf5ac84b9cb1fc1b84b00deca94c569
文昊 胡 <hwh19981124@hotmail.com>
2019-09-26 08:28:24 +0800

    change some

--- src/test/client/Client.java
+++ src/test/client/Client.java
@@ -3,11 +3,14 @@ package test.client;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import test.dao.CustomerDao;
+import test.service.CustomerService;
+import test.service.CustomerServiceImpl;
 
 public class Client {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext=new ClassPathXmlApplicationContext("spring-config.xml");
-       CustomerDao ob= (CustomerDao)classPathXmlApplicationContext.getBean("CustomerDao");
-       ob.saveCustomer();
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        CustomerService customerService = (CustomerServiceImpl) classPathXmlApplicationContext.getBean("CustomerService");
+       // CustomerDao customerDao = (CustomerDao) classPathXmlApplicationContext.getBean("CustomerDao");
+        customerService.saveCustomer();
     }
 }
--- src/test/service/CustomerServiceImpl.java
+++ src/test/service/CustomerServiceImpl.java
@@ -1,10 +1,15 @@
 package test.service;
 
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import test.dao.CustomerDao;
+import test.dao.CustomerDaoImpl;
 
 public class CustomerServiceImpl implements CustomerService {
-    @Override
+
+    private CustomerDao customerDaoImpl=new CustomerDaoImpl();
     public void saveCustomer() {
         System.out.println("Service");
+        customerDaoImpl.saveCustomer();
     }
 }
